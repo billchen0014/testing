423f57f90b04beb3ca094fa66c6795c9c712fd63
Bill Chen <sche0034@student.monash.edu>
2019-12-06 09:24:00 +1100

    adding google maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AtHeaderComponent } from './at-header/at-header.component';
+import { MapsComponent } from './maps/maps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AtHeaderComponent,
+    MapsComponent,
   ],
   imports: [
     BrowserModule,
