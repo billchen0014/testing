374b31080e498a73ef3f55fe296d2d368e760589
Bill Chen <billchenmulgrave@gmail.com>
2019-12-06 16:23:41 +1100

    google maps working now

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.css']
 })
-export class MapsComponent implements OnInit {
+export class MapsComponent implements AfterViewInit{
+@ViewChild('gmap',{static: false}) 
+gmapElement: any;
 
+  map: google.maps.Map;
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    var mapProp = {
+      center: new  google.maps.LatLng(-37.956864, 145.237495),
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
 }
